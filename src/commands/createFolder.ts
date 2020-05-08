4241a62ff49998bea1254c76075aff4740564313
X1 Carbon <yjl9903@vip.qq.com>
2020-05-08 23:48:55 +0800

    add: shortcut

--- src/commands/createFolder.ts
+++ src/commands/createFolder.ts
@@ -3,8 +3,8 @@ import { generateFolder } from '../services/folder';
 import { getScheme } from '../services';
 
 @command('create <name>', 'open settings file')
-@option('--folder <folderName>', 'create in sub folder')
-@option('--scheme <schemeName>', 'choose scheme template')
+@option('-f, --folder <folderName>', 'create in sub folder')
+@option('-s, --scheme <schemeName>', 'choose scheme template')
 export class createFolderCommand extends AbstractCommand {
   async action(name: string, option: { scheme?: string; folder?: string }) {
     generateFolder(name, getScheme(option.scheme), option.folder);
